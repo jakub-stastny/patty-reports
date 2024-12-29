5dc049a71fc513ce448356066e1a9d84884d5702
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-12-29 11:54:40 -0600

    Initial import

---
+++ .env/emacs.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path (expand-file-name "~/.zsh/environments/elisp"))
+
+;; (load-theme 'tsdh-light)
+
+(load "literate-programming")
+
+;; M-q to break paragraph. Use EVIL's J command to join.
+(set-fill-column 80)
