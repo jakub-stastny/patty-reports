de25a46cf3cae7330f90450a2ec15cbe6293eabd
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-01-07 12:51:27 -0600

    Given the Bubble format, no need to see it pretty

--- .env/zsh.zsh
+++ .env/zsh.zsh
@@ -5,7 +5,7 @@ load ~/.zsh/environments/basic.zsh
 alias emacs="emacs --load $PWD/.env/emacs.el"
 
 function report () {
-  curl http://localhost:8080/api/v1/reports/$1 --json @$2 | jq
+  curl http://localhost:8080/api/v1/reports/$1 --json @$2 #| jq
 }
 
 report-custom-functions
