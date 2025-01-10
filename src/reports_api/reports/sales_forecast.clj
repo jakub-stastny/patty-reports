9789096b178d327ba2690351b136ccc5e152a8d1
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2025-01-10 23:05:20 +0000

    Add starting-customers

--- src/reports_api/reports/sales_forecast.clj
+++ src/reports_api/reports/sales_forecast.clj
@@ -37,6 +37,7 @@
         (merge (validate-or-default :average-eu-vat-rate [(v/generate-range-validator 0 1)] 0))
         (merge (validate-or-default :bad-debt-provision [(v/generate-range-validator 0 1)] 0))
         (merge (validate-or-default :eu-vat-approach [eu-vat-approach-validator] :own-vat-returns))
+        (merge (validate-or-default :starting-customers [v/number-validator] 0))
         (merge (validate-or-default :registered-for-vat [v/boolean-validator] true))
         (merge (validate-or-default :vat-payment-months [v/optional-single-or-multiple-months-validator] nil)))))
 
