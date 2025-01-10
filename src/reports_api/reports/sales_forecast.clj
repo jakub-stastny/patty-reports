ce6483d4c31c8d7cab408a0554eb66e9b9d007d1
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2025-01-10 18:34:22 +0000

    Customer VAT status

--- src/reports_api/reports/sales_forecast.clj
+++ src/reports_api/reports/sales_forecast.clj
@@ -3,11 +3,23 @@
             [reports-api.time :as t]
             [reports-api.validators :as v]))
 
+(def customer-vat-status-opts
+  {"All customers VAT registered" :all-vat-registered 
+   "Some customers VAT registered" :some-vat-registered})
+
+(def customer-vat-status-validator
+  (v/make-validator :customer-vat-status
+                   (str "must be one of: " (str/join "," (keys customer-vat-status-opts)))
+                   (fn [v]
+                     (when-let [matched-key (some #(and (= % v) %) (keys customer-vat-status-opts))]
+                       (get customer-vat-status-opts matched-key)))))
+
 (defn validate-inputs [inputs]
   (let [validate (fn [k validators] {k (v/validate inputs k validators)})
         validate-or-default (fn [k validators default] {k (v/validate-or-default inputs k validators default)})]
     (-> {}
         (merge (v/validate-projections-keys inputs))
+        (merge (validate-or-default :customer-vat-status [customer-vat-status-validator] :all-vat-registered))
         (merge (validate-or-default :average-eu-vat-rate [(v/generate-range-validator 0 1)] 0))
         (merge (validate-or-default :bad-debt-provision [(v/generate-range-validator 0 1)] 0)))))
 
