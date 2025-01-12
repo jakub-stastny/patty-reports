31ff44b755c99a72f497271fdfb27a3d36f6e061
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-01-12 17:50:43 -0600

    WIP tests

--- test/request_test.clj
+++ test/request_test.clj
@@ -1,36 +1,60 @@
-(println "XXX")
 (ns request-test
   (:require [clojure.test :refer :all]
+            [babashka.fs :as fs]
             [ring.mock.request :as mock]
             [clojure.edn :as edn]
             [cheshire.core :as json]
-            [clojure.java.io :as io]
             [reports-api.core :refer [app]]))
 
-(defn load-edn [path]
-  (with-open [rdr (io/reader path)]
-    (edn/read rdr)))
+(defn build-mock [endpoint data]
+  (assert (map? data) (str "Data must be a map, got" (pr-str data)))
 
-(defn load-json [path]
-  (with-open [rdr (io/reader path)]
-    (json/parse-stream rdr true)))
+  (-> (mock/request :post endpoint)
+      (mock/json-body (json/generate-string data))))
 
-(defn run-test-case [{:keys [label request-data-path expected-output]}]
+(defn run-test-case [endpoint {:keys [label request-data request-data-path expected-output] :as spec}]
+  (assert label (str "Test must have a label, got " (pr-str spec)))
+  (assert (or request-data request-data-path)
+          (str "Test must have either request-data or request-data-path, got " (pr-str spec)))
+
+  (println "~ Spec" (pr-str spec))
+
   (testing label
-    (let [request-data (load-json request-data-path)
-          response (app (mock/request :post "/api/v1/reports/staff-plan"
-                                      :content-type "application/json"
-                                      :body (json/generate-string request-data)))
-          actual-status (:status response)
-          actual-body (-> response :body (json/parse-string true))]
-      (is (= (:status expected-output) actual-status)
-          (str "Expected status " (:status expected-output)
-               " but got " actual-status))
-      (is (= (:body expected-output) actual-body)
-          (str "Expected response body " (:body expected-output)
-               " but got " actual-body)))))
+    (let [request-data (if request-data
+                         request-data
+                         (json/parse-string (slurp request-data-path)))
 
-(deftest test-staff-plan-endpoint
-  (let [test-specs (load-edn "tests/data/staff_plan.edn")]
-    (doseq [test-case test-specs]
-      (run-test-case test-case))))
+          response (app (build-mock endpoint request-data))]
+      (prn :rd request-data)
+      (println "~ Request payload" (pr-str request-data))
+      ;; (prn :resp response)
+      ))
+
+  ;; (testing label
+  ;;   (let [request-data (json/parse-string (slurp request-data-path))
+  ;;         response (app (mock/request :post "/api/v1/reports/staff-plan"
+  ;;                                     :content-type "application/json"
+  ;;                                     :body (json/generate-string request-data)))
+  ;;         actual-status (:status response)
+  ;;         actual-body (-> response :body (json/parse-string true))]
+  ;;     (is (= (:status expected-output) actual-status)
+  ;;         (str "Expected status " (:status expected-output)
+  ;;              " but got " actual-status))
+  ;;     (is (= (:body expected-output) actual-body)
+  ;;         (str "Expected response body " (:body expected-output)
+  ;;              " but got " actual-body))))
+  )
+
+(defn test-endpoint [{:keys [endpoint requests] :as spec}]
+  (assert endpoint (str "Test must have an endpoint, got " (pr-str spec)))
+  (assert (vector? requests)
+          (str "Test must have an array of requests, got " (pr-str spec)))
+
+  (println "➔" endpoint)
+  (doseq [r requests] (run-test-case endpoint r)))
+
+(deftest test-requests
+  (doseq [file (fs/glob "data/specs" "*.edn")]
+    (let [path (str file)]
+      (print (str "~ Running " path " "))
+      (test-endpoint (edn/read-string (slurp path))))))
