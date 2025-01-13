e996ae100bbae2ca0c7217f28f8a4ce7941359bc
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-01-13 17:48:49 -0600

    Print out in EDN, so I can copy & paste into the specs

--- utils/report_runner.clj
+++ utils/report_runner.clj
@@ -1,5 +1,16 @@
 (ns report-runner
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.string :as str]
+            [clojure.java.shell :refer [sh]]
+            [cheshire.core :as json]))
+
+(defn url [endpoint]
+  (str "http://localhost:8080" endpoint))
+
+(defn post [& chunks]
+  (println "\033[1;36m$\033[0m" (str/join " " chunks) "\n")
+  (let [{:keys [exit out err] :as results} (apply sh chunks)]
+    (prn (json/parse-string out true))))
 
 (defn -main
   ([path] (-main path nil))
@@ -10,5 +21,7 @@
          requests-to-process (if index [(nth requests index)] requests)]
      (doseq [{:keys [request-data request-data-path]} requests-to-process]
        (if request-data
-         (println (str "curl http://localhost:8080" endpoint " --json '" (pr-str request-data) "'"))
-         (println (str "curl http://localhost:8080" endpoint " --json @" request-data-path)))))))
+         (let [payload (json/generate-string request-data)]
+           (post "curl" (url endpoint) "--json" payload))
+         (post "curl" (url endpoint) "--json" (str "@" request-data-path)))))
+   (System/exit 0)))
