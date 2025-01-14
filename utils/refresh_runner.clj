66ac2e83321b9531caf7efee10e4347fcf951ce3
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-01-14 16:36:20 -0600

    Refresher works

--- utils/refresh_runner.clj
+++ utils/refresh_runner.clj
@@ -2,14 +2,12 @@
   (:require [clojure.edn :as edn]
             [clojure.string :as str]
             [clojure.pprint :refer [pprint]]
-            [clj-http.client :as http]
+            [babashka.http-client :as http]
             [cheshire.core :as json]))
 
 (defn post [endpoint payload]
   (http/post (str "http://localhost:8080" endpoint)
-             {:body payload
-              :content-type :json
-              :accept :json}))
+             {:body payload :throw false}))
 
 (defn format-edn [data]
   (with-out-str (pprint data)))
@@ -19,12 +17,18 @@
 
 (defn refresh-requests [{:keys [endpoint requests]}]
   (mapv (fn [{:keys [request-data request-data-path] :as request}]
-          (merge request {:expected-output {:status status :body body}})
-          (if request-data
-            (post endpoint (json/generate-string request-data))
-            (post endpoint (slurp request-data-path))))
+          (let [response
+                (if request-data
+                  (post endpoint (json/generate-string request-data))
+                  (post endpoint (slurp request-data-path)))
+
+                status (:status response)
+                body (json/parse-string (:body response) true)]
+            (merge request {:expected-output {:status status :body body}})))
         requests))
 
 (defn -main [path]
   (let [data (edn/read-string (slurp path))]
-    (save-edn path (merge data {:requests (refresh-requests data)}))))
+    (save-edn path (merge data {:requests (refresh-requests data)}))
+    (println "Done. Don't forget to verify the results running:")
+    (println "\n  clojure -M:test" path)))
