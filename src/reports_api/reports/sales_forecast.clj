cf063fb1fcc93df237aa6ac93c1ad1d1e90c2d91
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-02-04 16:18:06 -0600

    Helper rows easy & clean

--- src/reports_api/reports/sales_forecast.clj
+++ src/reports_api/reports/sales_forecast.clj
@@ -3,25 +3,13 @@
             [reports-api.helpers :as h]
             [reports-api.time :as t]
             [reports-api.reports.sales-forecast.validators :as v]
+            [reports-api.reports.sales-forecast.helper-rows :as hr]
             [reports-api.reports.sales-forecast.customer-rows :as cr]
             [reports-api.reports.sales-forecast.revenue-rows :as rr]
             [reports-api.reports.sales-forecast.vat-rows :as vr]
             [reports-api.xhelpers :as xh]
-            [reports-api.pro-rata-engine :as pr]
             [reports-api.bubble :as b]
             [reports-api.totals :as tot]))
-
-;; Bound to sales, not to customer number.
-(defn month-adjustment-ratios [{:keys [customer-activity-pattern]}]
-  (let [base-value (/ 1.0 12)] ; The value for even distribution
-    (mapv #(/ % base-value) customer-activity-pattern)))
-
-(defn helper-rows [prev-months month {:keys [yoy-growth-rate sales-start-date sales-end-date] :as inputs}]
-  (let [year-index (int (/ (count prev-months) 12))
-        sales-growth-rate (nth yoy-growth-rate year-index)
-        seasonal-adjustment-rate (nth (month-adjustment-ratios inputs) (dec (:month month)))
-        pro-rata-factor (pr/pro-rata-factor (pr/calculate-pro-rata-factor month sales-start-date sales-end-date))]
-    {:sales-growth-rate sales-growth-rate :seasonal-adjustment-rate seasonal-adjustment-rate :pro-rata-factor pro-rata-factor}))
 
 ;; TODO: Review all of these and get rid of the multi-key merge (rather than assoc) properties.
 (def ^:private row-namespaces
@@ -36,11 +24,10 @@
 ;;     (merge results (rr/revenue-rows prev-months month inputs results))
 ;;     (merge results (rr/sales-revenue-rows prev-months month inputs results))))
 (defn generate-report-month [prev-months month inputs]
-  (let [processing-fns (keep #(ns-resolve % 'rows) row-namespaces)]  ;; Resolve `rows` from each namespace
+  (let [processing-fns (keep #(ns-resolve % 'process) row-namespaces)]
     (reduce (fn [results f]
-              (merge results (f prev-months month inputs)))
-            (helper-rows prev-months month inputs)
-            processing-fns)))
+              (merge results (f prev-months month inputs results)))
+            {} processing-fns)))
 
 (def row-ns-props
   (reduce (fn [acc namespace]
--- src/reports_api/reports/sales_forecast/helper_rows.clj
+++ src/reports_api/reports/sales_forecast/helper_rows.clj
@@ -1,15 +1,34 @@
 (ns reports-api.reports.sales-forecast.helper-rows
   (:require [clojure.string :as str]
             [reports-api.helpers :as h]
+            [reports-api.pro-rata-engine :as pr]
             [reports-api.time :as t]))
 
-(defn calculate-xxx [inputs results]
-  (let [{:keys [registered-for-helper eu-vat-approach percentage-eu-customers-vat-registered sales-vat average-eu-vat-rate]} inputs
-        {:keys [domestic-sales eu-sales]} results]
-    ;; TODO
-    ;; (if registered-for-helper)
-    ))
+;; This was the original code:
+;;
+;; (defn helper-rows [prev-months month {:keys [yoy-growth-rate sales-start-date sales-end-date] :as inputs}]
+;;   (let [year-index (int (/ (count prev-months) 12))
+;;         sales-growth-rate (nth yoy-growth-rate year-index)
+;;         seasonal-adjustment-rate (nth (month-adjustment-ratios inputs) (dec (:month month)))]
+;;     {:sales-growth-rate sales-growth-rate :seasonal-adjustment-rate seasonal-adjustment-rate :pro-rata-factor pro-rata-factor}))
+
+(defn calculate-sales-growth-rate [prev-months _ {:keys [yoy-growth-rate]}]
+  (let [year-index (int (/ (count prev-months) 12))]
+    (nth yoy-growth-rate year-index)))
+
+;; Bound to sales, not to customer number.
+(defn- month-adjustment-ratios [{:keys [customer-activity-pattern]}]
+  (let [base-value (/ 1.0 12)] ; The value for even distribution
+    (mapv #(/ % base-value) customer-activity-pattern)))
+
+(defn calculate-seasonal-adjustment-rate [prev-months month {:keys [yoy-growth-rate] :as inputs}]
+  (nth (month-adjustment-ratios inputs) (dec (:month month))))
+
+(defn calculate-pro-rata-factor [_ month {:keys [sales-start-date sales-end-date]}]
+  (pr/pro-rata-factor (pr/calculate-pro-rata-factor month sales-start-date sales-end-date)))
 
 (defn process [prev-months month inputs results]
-  (as-> results results
-    (assoc results :xxx (calculate-xxx inputs results))))
+  (as-> results r
+    (assoc r :sales-growth-rate (calculate-sales-growth-rate prev-months month inputs))
+    (assoc r :seasonal-adjustment-rate (calculate-seasonal-adjustment-rate prev-months month inputs))
+    (assoc r :pro-rata-factor (calculate-pro-rata-factor prev-months month inputs))))
