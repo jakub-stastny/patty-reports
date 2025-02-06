190996dc43b0216aff9fda9bc7c43d1e6fbf6ef3
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-02-06 15:19:35 -0600

    Bad debts

--- src/reports_api/reports/sales_forecast/cash_flow_rows.clj
+++ src/reports_api/reports/sales_forecast/cash_flow_rows.clj
@@ -11,7 +11,15 @@
     ;; (h/assert-number fn-name :total-revenue total-revenue)
     (:total-revenue (or (get prev-months offset) {:total-revenue 0}))))
 
+(h/defn-pass-name calculate-bad-debts [fn-name inputs results]
+  (let [;;{:keys [bad-debt-provision]} inputs
+        {:keys [sales-revenue-due bad-debt-provision]} results]
+    (h/assert-number fn-name :sales-revenue-due sales-revenue-due)
+    (h/assert-number fn-name :bad-debt-provision bad-debt-provision)
+    (* -1 revenue-due bad-debt-provision)))
+
 (defn process [prev-months month inputs results]
   ;; TODO: Cost (from the helpers Claude).
   (as-> (merge {:cost 1} results) r
-    (assoc r :sales-revenue-due (calculate-sales-revenue-due prev-months inputs r))))
+    (assoc r :sales-revenue-due (calculate-sales-revenue-due prev-months inputs r))
+    (assoc r :bad-debts (calculate-bad-debts inputs r))))
