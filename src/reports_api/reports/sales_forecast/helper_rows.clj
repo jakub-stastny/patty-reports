f4f19d6e69260f5725a39cea63b39f43fcde5f26
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-02-06 16:01:28 -0600

    Use the calculate-properties API in helper rows

--- src/reports_api/reports/sales_forecast/helper_rows.clj
+++ src/reports_api/reports/sales_forecast/helper_rows.clj
@@ -12,7 +12,7 @@
 ;;         seasonal-adjustment-rate (nth (month-adjustment-ratios inputs) (dec (:month month)))]
 ;;     {:sales-growth-rate sales-growth-rate :seasonal-adjustment-rate seasonal-adjustment-rate :pro-rata-factor pro-rata-factor}))
 
-(defn calculate-sales-growth-rate [prev-months _ {:keys [yoy-growth-rate]}]
+(defn calculate-sales-growth-rate [prev-months _ {:keys [yoy-growth-rate]} _]
   (let [year-index (int (/ (count prev-months) 12))]
     (nth yoy-growth-rate year-index)))
 
@@ -21,14 +21,14 @@
   (let [base-value (/ 1.0 12)] ; The value for even distribution
     (mapv #(/ % base-value) customer-activity-pattern)))
 
-(defn calculate-seasonal-adjustment-rate [prev-months month {:keys [yoy-growth-rate] :as inputs}]
+(defn calculate-seasonal-adjustment-rate [prev-months month {:keys [yoy-growth-rate] :as inputs} _]
   (nth (month-adjustment-ratios inputs) (dec (:month month))))
 
-(defn calculate-pro-rata-factor [_ month {:keys [sales-start-date sales-end-date]}]
+(defn calculate-pro-rata-factor [_ month {:keys [sales-start-date sales-end-date]} _]
   (pr/pro-rata-factor (pr/calculate-pro-rata-factor month sales-start-date sales-end-date)))
 
 (defn process [prev-months month inputs results]
-  (as-> results r
-    (assoc r :sales-growth-rate (calculate-sales-growth-rate prev-months month inputs))
-    (assoc r :seasonal-adjustment-rate (calculate-seasonal-adjustment-rate prev-months month inputs))
-    (assoc r :pro-rata-factor (calculate-pro-rata-factor prev-months month inputs))))
+  (h/calculate-properties
+   'reports-api.reports.sales-forecast.helper-rows
+   [:sales-growth-rate :seasonal-adjustment-rate :pro-rata-factor]
+   results prev-months month inputs))
