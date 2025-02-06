29a71d77d73c31484a3ee054987978db54197bab
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-02-06 14:39:29 -0600

    Bad debt provision

--- src/reports_api/reports/sales_forecast/cost_and_profit_rows.clj
+++ src/reports_api/reports/sales_forecast/cost_and_profit_rows.clj
@@ -10,7 +10,15 @@
     (h/assert-number fn-name :cost cost)
     (* units-sold cost)))
 
+(h/defn-pass-name calculate-bad-debt-provision [fn-name inputs results]
+  (let [{:keys [bad-debt-provision]} inputs
+        {:keys [total-revenue]} results]
+    (h/assert-number fn-name :bad-debt-provision bad-debt-provision)
+    (h/assert-number fn-name :total-revenue total-revenue)
+    (* -1 bad-debt-provision total-revenue)))
+
 (defn process [prev-months month inputs results]
   ;; TODO: Cost (from the helpers Claude).
   (as-> (merge {:cost 1} results) r
-    (assoc r :cost-of-sales (calculate-cost-of-sales inputs r))))
+    (assoc r :cost-of-sales (calculate-cost-of-sales inputs r))
+    (assoc r :bad-debt-provision (calculate-bad-debt-provision inputs r))))
