3b65c08e5e534df2abb4310ad98cf31e6e12acf9
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-02-10 07:00:52 -0600

    Ignore helper keys

--- src/reports_api/bubble.clj
+++ src/reports_api/bubble.clj
@@ -2,10 +2,15 @@
   (:require [clojure.string :as str]
             [reports-api.helpers :as h]))
 
+(def ignored-keys #{:month :relative-month :being-sold :sales-growth-rate
+                    :seasonal-adjustment-rate :pro-rata-factor})
+
 (defn format-for-bubble-one [results keys]
   (reduce (fn [acc report-month-data]
             (reduce (fn [acc key]
-                      (update acc key conj (h/round (h/get! report-month-data key))))
+                      (if (ignored-keys key)
+                        acc
+                        (update acc key conj (h/round (h/get! report-month-data key)))))
                     acc keys))
           (zipmap keys (repeat []))
           results))
